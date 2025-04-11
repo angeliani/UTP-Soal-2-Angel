c615341d34f2b5931e889efd3df84c009a03663e
angel <angeliani.junitaviana@gmail.com>
2025-04-11 20:21:04 +0700

    Add files via upload

---
+++ src/Seni.java
@@ -0,0 +1,13 @@
+class Seni extends BarangLelang {
+    private String seniman;
+
+    public Seni(String namaBarang, double hargaAwal, String seniman) {
+        super(namaBarang, hargaAwal);
+        this.seniman = seniman;
+    }
+
+    public void mulaiLelang() {
+        super.mulaiLelang();
+        System.out.println("Karya seni ini dibuat oleh: " + seniman);
+    }
+}
